308b7f4cf6ba9db4031fd0351c1d80dfcc035730
Raul <76450597+rbayas97@users.noreply.github.com>
2022-12-29 15:19:48 -0500

    Add while loop for user input if greater than 100

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let container = document.querySelector('container');
 
 function createGrid() {
-    let numberGrid = prompt("Enter a number for your grid dimension. 1-100");
+    let numberGrid = prompt("Enter a number for your grid dimensions. 1-100");
+    while (numberGrid >= 101){
+        numberGrid = prompt("Enter a number for your grid dimensions. 1-100");
+    }
     for (let i = 0; i < numberGrid; i++){
         let columns = document.createElement('div');
         columns.classList.add('column');
